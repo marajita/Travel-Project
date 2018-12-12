8ffd221c479c888942ab0c5ab5952b5445b2303f
ajmesa <amesa987@gmail.com>
2018-12-12 18:21:48 -0500

    Added data retrieval from firebase and made signing in and out more user friendly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -635,6 +635,13 @@ var DISPLAY_DATA = {
 };
 
 $(document).ready(function() {
+  $("#btnLogOut").hide();
+  //Hides login or logout buttons depending on whether someone is signed in
+  $(".btn-action").on("click", function() {
+    var user = firebase.auth().currentUser;
+    console.log(user);
+  });
+
   $("#btnLogIn").on("click", function() {
     var userEmail = $("#txtEmail").val();
     var userPassword = $("#txtPassword").val();
@@ -648,13 +655,28 @@ $(document).ready(function() {
         console.log(errorCode);
         console.log(errorMessage);
         // ...
+      })
+      .then(function() {
+        var user = firebase.auth().currentUser;
+        $("#current-user").text(user.email);
+        if (user === null) {
+          $("#btnLogIn").show();
+          $("#btnLogOut").hide();
+          $("#btnSignUp").show();
+          $("#txtEmail").show();
+          $("#txtPassword").show();
+        } else {
+          $("#btnLogIn").hide();
+          $("#btnLogOut").show();
+          $("#btnSignUp").hide();
+          $("#txtEmail").hide();
+          $("#txtPassword").hide();
+        }
+        /*
+          const promise = auth.signInWithNameAndPassword(userEmail, userPassword);
+          promise.catch(e => console.log(e.message));
+        */
       });
-    var user = firebase.auth().currentUser;
-    $("#current-user").text(user.email);
-    /*
-    const promise = auth.signInWithNameAndPassword(userEmail, userPassword);
-    promise.catch(e => console.log(e.message));
-    */
   });
 
   $("#btnLogOut").on("click", function() {
@@ -666,8 +688,16 @@ $(document).ready(function() {
       })
       .catch(function(error) {
         // An error happened.
+      })
+      .then(function() {
+        $("#current-user").text("");
+        $("#btnLogIn").show();
+        $("#btnLogOut").hide();
+        $("#btnSignUp").show();
+        $("#txtEmail").show();
+        $("#txtPassword").show();
+        $("#firebase-retrieval").text("");
       });
-    $("#current-user").text("");
   });
 
   $("#btnSignUp").on("click", function() {
@@ -684,31 +714,56 @@ $(document).ready(function() {
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log(errorCode);
-        console.log(error.Message);
+        console.log(errorMessage);
         console.log("Something Went Wrong");
         // ...
+      })
+      .then(function() {
+        var user = firebase.auth().currentUser;
+        $("#current-user").text(user.email);
+        if (user === null) {
+          $("#btnLogIn").show();
+          $("#btnLogOut").hide();
+          $("#btnSignUp").show();
+          $("#txtEmail").show();
+          $("#txtPassword").show();
+        } else {
+          $("#btnLogIn").hide();
+          $("#btnLogOut").show();
+          $("#btnSignUp").hide();
+          $("#txtEmail").hide();
+          $("#txtPassword").hide();
+        }
+        /*
+          const promise = auth.createUserWithNameAndPassword(userEmail, userPassword);
+          promise.catch(e => console.log(e.message));
+        */
       });
-    var user = firebase.auth().currentUser;
-    $("#current-user").text(user.email);
-    /*
-    const promise = auth.createUserWithNameAndPassword(userEmail, userPassword);
-    promise.catch(e => console.log(e.message));
-    */
   });
 
-  $("#firebase-test").on("click", function() {
-    var userEmail = $("#current-user").text();
+  $("#firebase-send-test").on("click", function() {
+    var currentUserEmail = $("#current-user").text();
     var user = firebase.auth().currentUser;
+    var flightTest = TEST_DATA.flights[0].airline;
     console.log(user);
 
     database.ref("user/" + user.uid).update({
-      Email: userEmail,
-      Flight: TEST_DATA.flights[0]
+      Email: currentUserEmail,
+      Flight: flightTest
     });
 
-    console.log(userEmail);
+    console.log(currentUserEmail);
   });
-  //When doing the snapshot, use: database.ref("user/" + user.uid).once(function(snapshot){})
+
+  $("#firebase-retrieval-button").on("click", function() {
+    var user = firebase.auth().currentUser;
+    database.ref("user/" + user.uid).once("value", function(snapshot) {
+      //code goes here
+      currentUserEmail = snapshot.val().Email;
+      flightTest = snapshot.val().Flight;
+      $("#firebase-retrieval").text(currentUserEmail + flightTest);
+    });
+  });
 
   $(".ui-segment").hide();
   // Rajita changes
