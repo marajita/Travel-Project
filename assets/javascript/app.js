3b63c01e3636e278e5628456cd9e62343ad89439
ajmesa <amesa987@gmail.com>
2018-12-13 19:48:17 -0500

    Token Expiration Check 1799 Seconds Later

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,22 @@
+var intervalID;
+
+var timer = 0;
+function increaseTimer() {
+  timer++;
+  if (timer === 1799) {
+    //...
+  }
+}
+
+function runTimer() {
+  clearInterval(intervalID);
+  intervalID = setInterval(increaseTimer, 1000);
+}
+
+function stopTimer() {
+  clearInterval(intervalID);
+}
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyC3Bnvip6bvt2gN9kUa2FOiRvSaYfIisyk",
@@ -631,11 +650,31 @@ $(document).ready(function() {
   });
 
   $("#btnLogIn").on("click", function() {
+    event.preventDefault();
     var userEmail = $("#txtEmail").val();
     var userPassword = $("#txtPassword").val();
     firebase
       .auth()
       .signInWithEmailAndPassword(userEmail, userPassword)
+      .then(function(response) {
+        console.log(response);
+        var user = firebase.auth().currentUser;
+        console.log(user.email);
+        //   $("#current-user").text(user.email);
+        //   if (user === null) {
+        //     $("#btnLogIn").show();
+        //     $("#btnLogOut").hide();
+        //     $("#btnSignUp").show();
+        //     $("#txtEmail").show();
+        //     $("#txtPassword").show();
+        //   } else {
+        //     $("#btnLogIn").hide();
+        //     $("#btnLogOut").show();
+        //     $("#btnSignUp").hide();
+        //     $("#txtEmail").hide();
+        //     $("#txtPassword").hide();
+        //   }
+      })
       .catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
@@ -643,59 +682,56 @@ $(document).ready(function() {
         console.log(errorCode);
         console.log(errorMessage);
         // ...
-      })
-      .then(function() {
-        var user = firebase.auth().currentUser;
-        $("#current-user").text(user.email);
-        if (user === null) {
-          $("#btnLogIn").show();
-          $("#btnLogOut").hide();
-          $("#btnSignUp").show();
-          $("#txtEmail").show();
-          $("#txtPassword").show();
-        } else {
-          $("#btnLogIn").hide();
-          $("#btnLogOut").show();
-          $("#btnSignUp").hide();
-          $("#txtEmail").hide();
-          $("#txtPassword").hide();
-        }
-        /*
-          const promise = auth.signInWithNameAndPassword(userEmail, userPassword);
-          promise.catch(e => console.log(e.message));
-        */
       });
   });
 
   $("#btnLogOut").on("click", function() {
+    event.preventDefault();
     firebase
       .auth()
       .signOut()
-      .then(function() {
+      .then(function(response) {
+        console.log(response);
         // Sign-out successful.
+        // $("#current-user").text("");
+        // $("#btnLogIn").show();
+        // $("#btnLogOut").hide();
+        // $("#btnSignUp").show();
+        // $("#txtEmail").show();
+        // $("#txtPassword").show();
+        // $("#firebase-retrieval").text("");
       })
       .catch(function(error) {
         // An error happened.
-      })
-      .then(function() {
-        $("#current-user").text("");
-        $("#btnLogIn").show();
-        $("#btnLogOut").hide();
-        $("#btnSignUp").show();
-        $("#txtEmail").show();
-        $("#txtPassword").show();
-        $("#firebase-retrieval").text("");
       });
   });
 
-  $("#btnSignUp").on("click", function() {
+  $("#btnSignUp2").on("click", function() {
+    event.preventDefault();
     var userEmail = $("#txtEmail").val();
     var userPassword = $("#txtPassword").val();
+    console.log(userEmail);
     firebase
       .auth()
       .createUserWithEmailAndPassword(userEmail, userPassword)
       .then(function(response) {
         console.log(response);
+        var user = firebase.auth().currentUser;
+        console.log(user.email);
+        $("#current-user").text(user.email);
+        //   /*
+        //   if (user === null) {
+        //     $("#btnLogIn").show();
+        //     $("#btnLogOut").hide();
+        //     $("#btnSignUp").show();
+        //     $("#txtEmail").show();
+        //     $("#txtPassword").show();
+        //   } else {
+        //     $("#btnLogIn").hide();
+        //     $("#btnLogOut").show();
+        //     $("#btnSignUp").hide();
+        //     $("#txtEmail").hide();
+        //     $("#txtPassword").hide();
       })
       .catch(function(error) {
         // Handle Errors here.
@@ -705,27 +741,6 @@ $(document).ready(function() {
         console.log(errorMessage);
         console.log("Something Went Wrong");
         // ...
-      })
-      .then(function() {
-        var user = firebase.auth().currentUser;
-        $("#current-user").text(user.email);
-        if (user === null) {
-          $("#btnLogIn").show();
-          $("#btnLogOut").hide();
-          $("#btnSignUp").show();
-          $("#txtEmail").show();
-          $("#txtPassword").show();
-        } else {
-          $("#btnLogIn").hide();
-          $("#btnLogOut").show();
-          $("#btnSignUp").hide();
-          $("#txtEmail").hide();
-          $("#txtPassword").hide();
-        }
-        /*
-          const promise = auth.createUserWithNameAndPassword(userEmail, userPassword);
-          promise.catch(e => console.log(e.message));
-        */
       });
   });
 
